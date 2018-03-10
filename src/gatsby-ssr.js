4f947d775ff383802464d97fc79fa062495a1eb0
Mark Miyashita <negativetwelve@gmail.com>
2018-03-09 20:11:33 -0800

    Wrap the StyleSheetManager in a stateful component

--- src/gatsby-ssr.js
+++ src/gatsby-ssr.js
@@ -6,11 +6,18 @@ import {ServerStyleSheet, StyleSheetManager} from "styled-components"
 
 exports.replaceRenderer = ({bodyComponent, replaceBodyHTMLString, setHeadComponents}) => {
   const sheet = new ServerStyleSheet();
-  const App = (
-    <StyleSheetManager sheet={sheet.instance}>
-      {bodyComponent}
-    </StyleSheetManager>
-  );
+
+  // NOTE(mark): We must render a stateful component. Stateless components
+  // have an issue with rendering toString.
+  class App extends React.Component {
+    render() {
+      return (
+        <StyleSheetManager sheet={sheet.instance}>
+          {bodyComponent}
+        </StyleSheetManager>
+      );
+    }
+  }
 
   AppRegistry.registerComponent('App', () => App)
   const {element, stylesheets} = AppRegistry.getApplication('App');
