05a7606272f0c51ee3c0f414603fd8d38e30d6a5
Mark Miyashita <negativetwelve@gmail.com>
2018-09-28 14:29:18 -0700

    Fix initialProps on the root component

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -6,7 +6,7 @@ exports.wrapRootElement = ({element}) => {
   const Root = () => element;
 
   AppRegistry.registerComponent('Root', () => Root);
-  const application = AppRegistry.getApplication('Root', {initialProps: this.props});
+  const application = AppRegistry.getApplication('Root', {initialProps: {}});
 
   // Returns the wrapped element for the entire application.
   return application.element;
